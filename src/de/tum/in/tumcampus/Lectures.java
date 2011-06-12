801cc7fdd8a67fdaa779f0ed4a6c7ed379ba9b60
Thomas Bley <mails@thomasbley.de>
2011-06-12 17:57:31 +0000

    added new dummy activities

---
+++ src/de/tum/in/tumcampus/Lectures.java
@@ -0,0 +1,16 @@
+package de.tum.in.tumcampus;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Lectures extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lectures);
+
+        
+    }
+	
+}
--- src/de/tum/in/tumcampus/TumCampus.java
+++ src/de/tum/in/tumcampus/TumCampus.java
@@ -1,13 +1,82 @@
 package de.tum.in.tumcampus;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class TumCampus extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class TumCampus extends Activity implements OnItemClickListener {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.main);
+
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+
+		addItem(list, R.drawable.icon, "Vorlesungen", new Intent(this,
+				Lectures.class));
+
+		addItem(list, R.drawable.icon, "Speisepläne", new Intent(this,
+				Cafeterias.class));
+
+		addItem(list, R.drawable.icon, "MVV", new Intent(this,
+				Transports.class));
+
+		addItem(list, R.drawable.icon, "Nachrichten", new Intent(this,
+				News.class));
+
+		addItem(list, R.drawable.icon, "Veranstaltungen", new Intent(this,
+				Events.class));
+
+		addItem(list, R.drawable.icon, "Links", new Intent(this,
+				Links.class));
+
+		SimpleAdapter notes = new SimpleAdapter(this, list,
+				R.layout.main_listview, new String[] { "icon", "name",
+						"content" }, new int[] { R.id.icon, R.id.name });
+
+		ListView lv = (ListView) findViewById(R.id.listView1);
+		lv.setAdapter(notes);
+
+		lv.setOnItemClickListener(this);
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+
+		ListView lv = (ListView) findViewById(R.id.listView1);
+		ListAdapter adapter = lv.getAdapter();
+
+		Map<String, Object> map = (Map<String, Object>) adapter
+				.getItem(position);
+
+		Intent itemIntent = (Intent) map.get("intent");
+		startActivity(itemIntent);
+	}
+
+	private void addItem(List<Map<String, Object>> data, int icon, String name,
+			Intent intent) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("icon", icon);
+		map.put("name", name);
+		map.put("intent", intent);
+		data.add(map);
+	}
 }
