7ec00c568d40c0bdc3ffd5805b013ed66193e447
Thomas Bley <thbley+github@gmail.com>
2011-06-23 20:03:40 +0000

    changed dates to skip yesterday, added group by name (esp. beilagen)

--- src/de/tum/in/tumcampus/Cafeterias.java
+++ src/de/tum/in/tumcampus/Cafeterias.java
@@ -1,5 +1,8 @@
 package de.tum.in.tumcampus;
 
+import java.util.HashMap;
+import java.util.List;
+
 import android.app.Activity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -9,9 +12,11 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.SimpleCursorAdapter;
 import android.widget.SlidingDrawer;
 import android.widget.TextView;
+import de.tum.in.tumcampus.models.CafeteriaMenuManager;
 
 public class Cafeterias extends Activity implements OnItemClickListener {
 
@@ -39,7 +44,7 @@ public class Cafeterias extends Activity implements OnItemClickListener {
 
 		Cursor c = db.rawQuery("SELECT DISTINCT date, date as _id "
 				+ "FROM cafeterias_menus WHERE "
-				+ "date >= date('now', '-1 day') ORDER BY date", null);
+				+ "date >= date('now') ORDER BY date", null);
 
 		ListAdapter adapter = new SimpleCursorAdapter(this,
 				android.R.layout.simple_list_item_1, c, c.getColumnNames(),
@@ -89,17 +94,21 @@ public class Cafeterias extends Activity implements OnItemClickListener {
 			TextView tv = (TextView) findViewById(R.id.cafeteriaText);
 			tv.setText(mensaName + ": " + date);
 
-			Cursor c = db.rawQuery("SELECT typeLong, name, id as _id "
-					+ "FROM cafeterias_menus WHERE mensaId = ? AND "
-					+ "date = ? ORDER BY typeNr, typeShort", new String[] {
-					mensaId, date });
+			CafeteriaMenuManager cmm = new CafeteriaMenuManager(this,
+					"database.db");
+			List<HashMap<String, String>> list = cmm.getFromDb(mensaId, date);
+			cmm.close();
 
-			// TODO group by typeLong ??
-			// TODO Öffnungszeiten
-			ListAdapter adapter = new SimpleCursorAdapter(this,
-					android.R.layout.two_line_list_item, c,
-					c.getColumnNames(), new int[] { android.R.id.text1,
-							android.R.id.text2 }) {
+			String[] from = new String[] {};
+			if (list.size() > 0) {
+				from = list.get(0).keySet().toArray(new String[] {});
+			}
+			int[] to = { android.R.id.text1, android.R.id.text2 };
+
+			ListView lv3 = (ListView) findViewById(R.id.listView3);
+
+			SimpleAdapter adapter = new SimpleAdapter(this, list,
+					android.R.layout.two_line_list_item, from, to) {
 				public boolean areAllItemsEnabled() {
 					return false;
 				}
@@ -108,9 +117,9 @@ public class Cafeterias extends Activity implements OnItemClickListener {
 					return false;
 				}
 			};
-
-			ListView lv3 = (ListView) findViewById(R.id.listView3);
 			lv3.setAdapter(adapter);
+
+			// TODO Öffnungszeiten
 		}
 	}
 }
