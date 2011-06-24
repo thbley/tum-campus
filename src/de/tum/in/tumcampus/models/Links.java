b26b61bb4a6a3ef41d38f261ab557f16bd2968d9
Thomas Bley <mails@thomasbley.de>
2011-06-24 07:57:33 +0000

    added icon

--- src/de/tum/in/tumcampus/models/Links.java
+++ src/de/tum/in/tumcampus/models/Links.java
@@ -4,15 +4,17 @@ public class Links {
 	int id;
 	String name;
 	String url;
+	String icon;
 
-	public Links(int id, String name, String url) {
+	public Links(int id, String name, String url, String icon) {
 		this.id = id;
 		this.name = name;
 		this.url = url;
+		this.icon = icon;
 	}
 
 	public String toString() {
 		return "id=" + this.id + " name=" + this.name + " url="
-				+ this.url;
+				+ this.url + " icon="+icon;
 	}
 }
