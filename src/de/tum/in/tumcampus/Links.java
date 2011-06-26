8070ebb9ad538e8f9ee63caac40d6921cfa591d3
Thomas Bley <thbley+github@gmail.com>
2011-06-26 08:28:48 +0000

    formatting

--- src/de/tum/in/tumcampus/Links.java
+++ src/de/tum/in/tumcampus/Links.java
@@ -9,8 +9,5 @@ public class Links extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.links);
-
-        
     }
-	
 }
