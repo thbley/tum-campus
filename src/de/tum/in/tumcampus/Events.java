1356f535571c1c521ec53801973a573dcdc1f00a
Thomas Bley <mails@thomasbley.de>
2011-06-28 16:45:46 +0000

    moved db stuff to manager

--- src/de/tum/in/tumcampus/Events.java
+++ src/de/tum/in/tumcampus/Events.java
@@ -1,13 +1,7 @@
 package de.tum.in.tumcampus;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import android.app.Activity;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -15,67 +9,41 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.widget.SimpleCursorAdapter;
+import android.widget.SimpleCursorAdapter.ViewBinder;
 import android.widget.SlidingDrawer;
 import android.widget.TextView;
+import de.tum.in.tumcampus.models.EventManager;
 
-public class Events extends Activity implements OnItemClickListener {
-
-	SQLiteDatabase db;
+public class Events extends Activity implements OnItemClickListener, ViewBinder {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.events);
 
-		db = SQLiteDatabase.openDatabase(this.getDatabasePath("database.db")
-				.toString(), null, SQLiteDatabase.OPEN_READONLY);
+		EventManager em = new EventManager(this, "database.db");
+		Cursor c = em.getAllFromDb();
 
-		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
-
-		// TODO move to manager
-
-		String[] weekDays = "So,Mo,Di,Mi,Do,Fr,Sa".split(",");
-
-		Cursor c = db
-				.rawQuery(
-						"SELECT image, name, strftime('%w', start_time), strftime('%d.%m.%Y %H:%M', start_time), "
-								+ "strftime('%H:%M', end_time), "
-								+ "location, description, id as _id "
-								+ "FROM events WHERE end_time > datetime() "
-								+ "ORDER BY start_time ASC LIMIT 25", null);
-
-		while (c.moveToNext()) {
-			Map<String, Object> map = new HashMap<String, Object>();
-
-			map.put("image", c.getString(0));
-			map.put("name", c.getString(1));
-			map.put("infos", weekDays[c.getInt(2)] + ", " + c.getString(3)
-					+ " - " + c.getString(4) + "\n" + c.getString(5));
-			map.put("description", c.getString(6));
-			map.put("id", c.getString(7));
-			list.add(map);
-		}
-		c.close();
-
-		SimpleAdapter adapter;
-		adapter = new SimpleAdapter(this, list, R.layout.events_listview,
-				new String[] { "image", "name", "infos" }, new int[] {
+		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+				R.layout.events_listview, c, c.getColumnNames(), new int[] {
 						R.id.icon, R.id.title, R.id.infos });
+		adapter.setViewBinder(this);
 
 		ListView lv = (ListView) findViewById(R.id.listView);
 		lv.setAdapter(adapter);
 		lv.setOnItemClickListener(this);
+		em.close();
 	}
 
 	@Override
 	public void onItemClick(AdapterView<?> av, View v, int position, long id) {
+		
+		// TODO move to activity
 		ListView lv = (ListView) findViewById(R.id.listView);
-		@SuppressWarnings("unchecked")
-		Map<String, Object> map = (Map<String, Object>) lv.getAdapter()
-				.getItem(position);
-		String description = (String) map.get("description");
-		String image = (String) map.get("image");
+		Cursor c = (Cursor) lv.getAdapter().getItem(position);
+		String description = c.getString(c.getColumnIndex("description"));
+		String image = c.getString(c.getColumnIndex("image"));
 
 		SlidingDrawer sd = (SlidingDrawer) findViewById(R.id.slidingDrawer1);
 		if (!sd.isOpened()) {
@@ -93,5 +61,21 @@ public class Events extends Activity implements OnItemClickListener {
 				/ (double) iv.getDrawable().getIntrinsicHeight();
 		iv.getLayoutParams().width = 300;
 		iv.getLayoutParams().height = (int) Math.floor(300 / ratio);
+	}
+
+	@Override
+	public boolean setViewValue(View view, Cursor c, int index) {
+
+		String[] weekDays = "So,Mo,Di,Mi,Do,Fr,Sa".split(",");
+
+		if (view.getId() == R.id.infos) {
+			TextView infos = (TextView) view;
+			infos.setText(weekDays[c.getInt(c.getColumnIndex("weekday"))]
+					+ ", " + c.getString(c.getColumnIndex("start")) + " - "
+					+ c.getString(c.getColumnIndex("end")) + "\n"
+					+ c.getString(c.getColumnIndex("location")));
+			return true;
+		}
+		return false;
 	}
 }
