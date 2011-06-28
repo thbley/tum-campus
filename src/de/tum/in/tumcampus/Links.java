eb392c932d7eabefc315d072a9e893e33649d8fe
Christoph Richter <mails@thomasbley.de>
2011-06-28 20:39:10 +0000

    changed colors for main screen

--- src/de/tum/in/tumcampus/Links.java
+++ src/de/tum/in/tumcampus/Links.java
@@ -1,10 +1,13 @@
 package de.tum.in.tumcampus;
 
 import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class Links extends Activity {
 
+	SQLiteDatabase db;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
