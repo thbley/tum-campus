9ec2e8aa55bdff41f7cf23b2c04e944b22ab7b5a
Thomas Bley <thbley+github@gmail.com>
2011-06-28 21:58:18 +0000

    added new gui tests

---
+++ src/de/tum/in/tumcampus/test/DatabaseTest.java
@@ -0,0 +1,30 @@
+package de.tum.in.tumcampus.test;
+
+import android.test.AndroidTestCase;
+
+public class DatabaseTest extends AndroidTestCase {
+
+	/*
+	private Database db;
+
+	protected void setUp() {
+		super.setUp();
+
+		db = new Database(this.getContext(), "test.db");
+		assertTrue(db.getWritableDatabase().isOpen());
+	}
+
+	protected void tearDown() {
+		db.close();
+		super.tearDown();
+	}
+
+	public final void testAddRunType() {
+		RunType rt = db.new RunType("Marathon2", 10);
+
+		assertTrue(db.getRunTypes().contains(rt));
+		
+		System.out.println("blabla " + db.getRunTypes().indexOf(rt));
+	}
+	*/
+}
---
+++ src/de/tum/in/tumcampus/test/DebugTest.java
@@ -0,0 +1,37 @@
+package de.tum.in.tumcampus.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.jayway.android.robotium.solo.Solo;
+
+import de.tum.in.tumcampus.TumCampus;
+
+public class DebugTest extends ActivityInstrumentationTestCase2<TumCampus> {
+
+	private Solo solo; // simulates the user of the app
+
+	public DebugTest() {
+		super("de.tum.in.tumcampus", TumCampus.class);
+	}
+
+	public void setUp() {
+		solo = new Solo(getInstrumentation(), getActivity());
+	}
+
+	public void testDebug() {
+		assertTrue(solo.searchText("Debug"));
+		solo.clickOnText("Debug");
+
+		assertTrue(solo.searchText("sqlite"));
+
+		solo.clickOnText("cafeterias");
+		solo.clickOnText("cafeterias_menus");
+		solo.clickOnText("feeds");
+		solo.clickOnText("feeds_items");
+		solo.clickOnText("links");
+		solo.clickOnText("events");
+		
+		solo.goBack();
+		assertTrue(solo.searchText("Hello World"));
+	}
+}
