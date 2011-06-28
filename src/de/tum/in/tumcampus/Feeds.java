ed1a063dac0d389bce46e575f9a10273d6f0ff34
Thomas Bley <mails@thomasbley.de>
2011-06-28 16:49:27 +0000

    moved db stuff to manager

--- src/de/tum/in/tumcampus/Feeds.java
+++ src/de/tum/in/tumcampus/Feeds.java
@@ -1,14 +1,8 @@
 package de.tum.in.tumcampus;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -16,13 +10,13 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.SimpleCursorAdapter;
+import android.widget.SimpleCursorAdapter.ViewBinder;
 import android.widget.SlidingDrawer;
+import de.tum.in.tumcampus.models.FeedItemManager;
+import de.tum.in.tumcampus.models.FeedManager;
 
-public class Feeds extends Activity implements OnItemClickListener {
-
-	SQLiteDatabase db;
+public class Feeds extends Activity implements OnItemClickListener, ViewBinder {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -32,11 +26,8 @@ public class Feeds extends Activity implements OnItemClickListener {
 		SlidingDrawer sd = (SlidingDrawer) findViewById(R.id.slidingDrawer1);
 		sd.open();
 
-		db = SQLiteDatabase.openDatabase(this.getDatabasePath("database.db")
-				.toString(), null, SQLiteDatabase.OPEN_READONLY);
-
-		Cursor c = db.rawQuery("SELECT DISTINCT name, feedUrl, id as _id "
-				+ "FROM feeds ORDER BY name", null);
+		FeedManager fm = new FeedManager(this, "database.db");
+		Cursor c = fm.getAllFromDb();
 
 		ListAdapter adapter = new SimpleCursorAdapter(this,
 				android.R.layout.simple_list_item_1, c, c.getColumnNames(),
@@ -45,8 +36,7 @@ public class Feeds extends Activity implements OnItemClickListener {
 		ListView lv = (ListView) findViewById(R.id.listView);
 		lv.setAdapter(adapter);
 		lv.setOnItemClickListener(this);
-
-		// TODO destroy db, cursor, move to manager
+		fm.close();
 	}
 
 	@Override
@@ -54,10 +44,8 @@ public class Feeds extends Activity implements OnItemClickListener {
 
 		if (av.getId() == R.id.listView2) {
 			ListView lv = (ListView) findViewById(R.id.listView2);
-			@SuppressWarnings("unchecked")
-			Map<String, Object> map = (Map<String, Object>) lv.getAdapter()
-					.getItem(position);
-			String link = (String) map.get("link");
+			Cursor c = (Cursor) lv.getAdapter().getItem(position);
+			String link = c.getString(c.getColumnIndex("link"));
 
 			Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
 			startActivity(intent);
@@ -76,54 +64,31 @@ public class Feeds extends Activity implements OnItemClickListener {
 
 		setTitle("Nachrichten: " + name);
 
-		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+		FeedItemManager fim = new FeedItemManager(this, "database.db");
+		Cursor c2 = fim.getAllFromDb(feedId);
 
-		// TODO move to manager
+		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+				R.layout.feeds_listview, c2, c2.getColumnNames(), new int[] {
+						R.id.icon, R.id.title, R.id.description });
 
-		Cursor c2 = db.rawQuery("SELECT image, title, description, link "
-				+ "FROM feeds_items " + "WHERE feedId = ? "
-				+ "ORDER BY date DESC", new String[] { feedId });
-
-		while (c2.moveToNext()) {
-			Map<String, Object> map = new HashMap<String, Object>();
-
-			String image = c2.getString(0);
-
-			if (image.length() == 0) {
-				image = String.valueOf(R.drawable.icon);
-			}
-
-			map.put("image", image);
-			map.put("title", c2.getString(1));
-			map.put("description", c2.getString(2));
-			map.put("link", c2.getString(3));
-			list.add(map);
-		}
-		c2.close();
-
-		// TODO optimize
-		boolean showImages = true;
-		if (list.size() > 1) {
-			showImages = !list.get(0).get("image")
-					.equals(String.valueOf(R.drawable.icon))
-					|| !list.get(1).get("image")
-							.equals(String.valueOf(R.drawable.icon));
-		}
-
-		SimpleAdapter adapter;
-		if (showImages) {
-			adapter = new SimpleAdapter(this, list, R.layout.feeds_listview,
-					new String[] { "image", "title", "description" },
-					new int[] { R.id.icon, R.id.title, R.id.description });
-		} else {
-			adapter = new SimpleAdapter(this, list,
-					R.layout.feeds_listview_text, new String[] { "title",
-							"description" }, new int[] { R.id.title,
-							R.id.description });
-		}
-
+		adapter.setViewBinder(this);
 		ListView lv2 = (ListView) findViewById(R.id.listView2);
 		lv2.setAdapter(adapter);
 		lv2.setOnItemClickListener(this);
+		fim.close();
+	}
+
+	@Override
+	public boolean setViewValue(View view, Cursor cursor, int index) {
+		// hide empty view elements
+		if (cursor.getString(index).length() == 0) {
+			view.setVisibility(View.GONE);
+
+			// no binding needed
+			return true;
+		} else {
+			view.setVisibility(View.VISIBLE);
+		}
+		return false;
 	}
 }
