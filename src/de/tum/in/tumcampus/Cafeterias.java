3a1826780c6cad568de3b896a96a31dd7ac8e066
Thomas Bley <mails@thomasbley.de>
2011-06-28 16:58:10 +0000

    moved db stuff to manager

--- src/de/tum/in/tumcampus/Cafeterias.java
+++ src/de/tum/in/tumcampus/Cafeterias.java
@@ -1,26 +1,23 @@
 package de.tum.in.tumcampus;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.Date;
 
 import android.app.Activity;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.SimpleCursorAdapter;
 import android.widget.SlidingDrawer;
 import android.widget.TextView;
+import de.tum.in.tumcampus.models.CafeteriaManager;
 import de.tum.in.tumcampus.models.CafeteriaMenuManager;
+import de.tum.in.tumcampus.models.Utils;
 
 public class Cafeterias extends Activity implements OnItemClickListener {
-
-	SQLiteDatabase db;
 
 	String date;
 	String dateStr;
@@ -31,22 +28,18 @@ public class Cafeterias extends Activity implements OnItemClickListener {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+		// TODO check again, next working day?
+		date = Utils.getDateString(new Date());
+		dateStr = Utils.getDateStringDe(new Date());
+
 		if (getResources().getConfiguration().orientation == android.content.res.Configuration.ORIENTATION_LANDSCAPE) {
 			setContentView(R.layout.cafeterias_horizontal);
 		} else {
 			setContentView(R.layout.cafeterias);
 		}
 
-		SlidingDrawer sd = (SlidingDrawer) findViewById(R.id.slidingDrawer1);
-		sd.open();
-
-		db = SQLiteDatabase.openDatabase(this.getDatabasePath("database.db")
-				.toString(), null, SQLiteDatabase.OPEN_READONLY);
-
-		Cursor c = db.rawQuery(
-				"SELECT DISTINCT strftime('%d.%m.%Y', date) as date_de, date as _id "
-						+ "FROM cafeterias_menus WHERE "
-						+ "date >= date() ORDER BY date", null);
+		CafeteriaMenuManager cmm = new CafeteriaMenuManager(this, "database.db");
+		Cursor c = cmm.getDatesFromDb();
 
 		ListAdapter adapter = new SimpleCursorAdapter(this,
 				android.R.layout.simple_list_item_1, c, c.getColumnNames(),
@@ -55,10 +48,10 @@ public class Cafeterias extends Activity implements OnItemClickListener {
 		ListView lv = (ListView) findViewById(R.id.listView);
 		lv.setAdapter(adapter);
 		lv.setOnItemClickListener(this);
+		cmm.close();
 
-		Cursor c2 = db.rawQuery("SELECT name, address, id as _id "
-				+ "FROM cafeterias "
-				+ "ORDER BY address like '%Garching%' desc, name", null);
+		CafeteriaManager cm = new CafeteriaManager(this, "database.db");
+		Cursor c2 = cm.getAllFromDb();
 
 		adapter = new SimpleCursorAdapter(this,
 				android.R.layout.two_line_list_item, c2, c2.getColumnNames(),
@@ -67,8 +60,7 @@ public class Cafeterias extends Activity implements OnItemClickListener {
 		ListView lv2 = (ListView) findViewById(R.id.listView2);
 		lv2.setAdapter(adapter);
 		lv2.setOnItemClickListener(this);
-
-		// TODO destroy db, cursor
+		cm.close();
 	}
 
 	@Override
@@ -83,12 +75,13 @@ public class Cafeterias extends Activity implements OnItemClickListener {
 			ListView lv = (ListView) findViewById(R.id.listView);
 			Cursor c = (Cursor) lv.getAdapter().getItem(position);
 			date = c.getString(c.getColumnIndex("_id"));
-			dateStr = c.getString(c.getColumnIndex("date"));
+			dateStr = c.getString(c.getColumnIndex("date_de"));
 		}
 
 		if (av.getId() == R.id.listView2) {
 			ListView lv2 = (ListView) findViewById(R.id.listView2);
 			Cursor c = (Cursor) lv2.getAdapter().getItem(position);
+
 			mensaId = c.getString(c.getColumnIndex("_id"));
 			mensaName = c.getString(c.getColumnIndex("name"));
 		}
@@ -99,20 +92,12 @@ public class Cafeterias extends Activity implements OnItemClickListener {
 
 			CafeteriaMenuManager cmm = new CafeteriaMenuManager(this,
 					"database.db");
-			List<HashMap<String, String>> list = cmm.getTypeNameFromDb(mensaId,
-					date);
-			cmm.close();
+			Cursor c = cmm.getTypeNameFromDb(mensaId, date);
 
-			String[] from = new String[] {};
-			if (list.size() > 0) {
-				from = list.get(0).keySet().toArray(new String[] {});
-			}
-			int[] to = { android.R.id.text1, android.R.id.text2 };
+			SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+					android.R.layout.two_line_list_item, c, c.getColumnNames(),
+					new int[] { android.R.id.text1, android.R.id.text2 }) {
 
-			ListView lv3 = (ListView) findViewById(R.id.listView3);
-
-			SimpleAdapter adapter = new SimpleAdapter(this, list,
-					android.R.layout.two_line_list_item, from, to) {
 				public boolean areAllItemsEnabled() {
 					return false;
 				}
@@ -121,7 +106,10 @@ public class Cafeterias extends Activity implements OnItemClickListener {
 					return false;
 				}
 			};
+
+			ListView lv3 = (ListView) findViewById(R.id.listView3);
 			lv3.setAdapter(adapter);
+			cmm.close();
 		}
 	}
 }
