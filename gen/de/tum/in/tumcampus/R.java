0623371d3f1df2cc1475b6ab81e721301823abf7
Thomas Bley <thbley+github@gmail.com>
2011-07-03 21:39:53 +0000

    update

--- gen/de/tum/in/tumcampus/R.java
+++ gen/de/tum/in/tumcampus/R.java
@@ -23,34 +23,34 @@ public final class R {
         public static final int zug=0x7f020009;
     }
     public static final class id {
-        public static final int cafeteriaText=0x7f050000;
-        public static final int content=0x7f050018;
-        public static final int debug=0x7f05000d;
-        public static final int debugCafeterias=0x7f050007;
-        public static final int debugCafeteriasMenus=0x7f050008;
-        public static final int debugEvents=0x7f05000c;
-        public static final int debugFeeds=0x7f050009;
-        public static final int debugFeedsItems=0x7f05000a;
-        public static final int debugLinks=0x7f05000b;
-        public static final int description=0x7f05000f;
-        public static final int handle=0x7f050004;
-        public static final int hello=0x7f050016;
-        public static final int icon=0x7f050010;
-        public static final int image=0x7f05000e;
-        public static final int imageView1=0x7f050014;
-        public static final int infos=0x7f050012;
-        public static final int listView=0x7f050006;
-        public static final int listView2=0x7f050002;
-        public static final int listView3=0x7f050001;
-        public static final int listViewMain=0x7f050017;
-        public static final int name=0x7f050013;
-        public static final int options=0x7f050005;
-        public static final int refresh=0x7f050015;
-        public static final int search=0x7f05001b;
-        public static final int slidingDrawer1=0x7f050003;
-        public static final int title=0x7f050011;
-        public static final int transportText=0x7f050019;
-        public static final int transportText2=0x7f05001a;
+        public static final int cafeteriaText=0x7f060000;
+        public static final int content=0x7f060018;
+        public static final int debug=0x7f06000d;
+        public static final int debugCafeterias=0x7f060007;
+        public static final int debugCafeteriasMenus=0x7f060008;
+        public static final int debugEvents=0x7f06000c;
+        public static final int debugFeeds=0x7f060009;
+        public static final int debugFeedsItems=0x7f06000a;
+        public static final int debugLinks=0x7f06000b;
+        public static final int description=0x7f06000f;
+        public static final int handle=0x7f060004;
+        public static final int hello=0x7f060016;
+        public static final int icon=0x7f060010;
+        public static final int image=0x7f06000e;
+        public static final int imageView1=0x7f060014;
+        public static final int infos=0x7f060012;
+        public static final int listView=0x7f060006;
+        public static final int listView2=0x7f060002;
+        public static final int listView3=0x7f060001;
+        public static final int listViewMain=0x7f060017;
+        public static final int name=0x7f060013;
+        public static final int options=0x7f060005;
+        public static final int refresh=0x7f060015;
+        public static final int search=0x7f06001b;
+        public static final int slidingDrawer1=0x7f060003;
+        public static final int title=0x7f060011;
+        public static final int transportText=0x7f060019;
+        public static final int transportText2=0x7f06001a;
     }
     public static final class layout {
         public static final int appinfo=0x7f030000;
@@ -73,5 +73,17 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+    }
+    public static final class style {
+        public static final int WindowTitleBackground=0x7f050000;
+        public static final int WindowTitleText=0x7f050001;
+        public static final int customListView=0x7f050002;
+        /**  TODO add title border, paddings 
+<item name="android:windowTitleBackgroundStyle">@style/WindowTitleBackground</item>
+		<item name="android:windowTitleStyle">@style/WindowTitleText</item>
+		<item name="android:textColor">#FF0000</item>
+		<item name="android:listViewStyle">@style/customListView</item>
+         */
+        public static final int customTheme=0x7f050003;
     }
 }
