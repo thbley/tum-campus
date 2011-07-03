28af06a3f4d587ce676bfe5d7b0d4bb04668428b
Thomas Bley <mails@thomasbley.de>
2011-07-03 21:41:36 +0000

    cleanup

--- src/de/tum/in/tumcampus/models/Feed.java
+++ src/de/tum/in/tumcampus/models/Feed.java
@@ -1,18 +1,16 @@
 package de.tum.in.tumcampus.models;
 
 public class Feed {
-	int id;
 	String name;
 	String feedUrl;
 
-	public Feed(int id, String name, String feedUrl) {
-		this.id = id;
+	public Feed(String name, String feedUrl) {
 		this.name = name;
 		this.feedUrl = feedUrl;
 	}
 
 	public String toString() {
-		return "id=" + this.id + " name=" + this.name + " feedUrl="
+		return "name=" + this.name + " feedUrl="
 				+ this.feedUrl;
 	}
 }
