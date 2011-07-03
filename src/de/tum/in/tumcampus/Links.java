5c6928e01232477b8338f3172b8c6e5fa9e28188
Thomas Bley <thbley+github@gmail.com>
2011-07-03 21:38:12 +0000

    added delete

--- src/de/tum/in/tumcampus/Links.java
+++ src/de/tum/in/tumcampus/Links.java
@@ -1,18 +1,22 @@
 package de.tum.in.tumcampus;
 
-import de.tum.in.tumcampus.models.LinkManager;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
-import android.widget.AdapterView.OnItemClickListener;
+import de.tum.in.tumcampus.models.LinkManager;
 
-public class Links extends Activity implements OnItemClickListener {
+public class Links extends Activity implements OnItemClickListener,
+		OnItemLongClickListener {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -29,6 +33,7 @@ public class Links extends Activity implements OnItemClickListener {
 		ListView lv = (ListView) findViewById(R.id.listView);
 		lv.setAdapter(adapter);
 		lv.setOnItemClickListener(this);
+		lv.setOnItemLongClickListener(this);
 		lm.close();
 	}
 
@@ -42,5 +47,40 @@ public class Links extends Activity implements OnItemClickListener {
 		// Connection to browser
 		Intent viewIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
 		startActivity(viewIntent);
+	}
+
+	@Override
+	public boolean onItemLongClick(AdapterView<?> av, View v, int position,
+			long id) {
+
+		final ListView lv = (ListView) findViewById(R.id.listView);
+		Cursor c = (Cursor) lv.getAdapter().getItem(position);
+		final String _id = c.getString(c.getColumnIndex("_id"));
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage("Wirklch löschen?");
+		builder.setPositiveButton("Ja", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+
+				LinkManager lm = new LinkManager(lv.getContext(), "database.db");
+				lm.deleteFromDb(_id);
+
+				SimpleCursorAdapter adapter = (SimpleCursorAdapter) lv
+						.getAdapter();
+				adapter.changeCursor(lm.getAllFromDb());
+				lm.close();
+
+				dialog.dismiss();
+			}
+		});
+		builder.setNegativeButton("Nein",
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.cancel();
+					}
+				});
+		builder.show();
+
+		return false;
 	}
 }
