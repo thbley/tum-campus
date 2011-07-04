c8e506d580ab9e12fc31e69b13f337f80be7eb40
Thomas Bley <thbley+github@gmail.com>
2011-07-04 03:04:31 +0000

    added news manager

---
+++ src/de/tum/in/tumcampus/models/News.java
@@ -0,0 +1,27 @@
+package de.tum.in.tumcampus.models;
+
+import java.util.Date;
+
+public class News {
+
+	String id;
+	String message;
+	String link;
+	String image;
+	Date date;
+
+	public News(String id, String message, String link, String image,
+			Date date) {
+
+		this.id = id;
+		this.message = message;
+		this.link = link;
+		this.image = image;
+		this.date = date;
+	}
+
+	public String toString() {
+		return "id=" + id + " message=" + message + " link=" + link + " iamge="
+				+ image + " date=" + Utils.getDateString(date);
+	}
+}
