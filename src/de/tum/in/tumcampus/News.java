4847d468de2e1c9678eeff2a231369281f967c54
Christoph Richter <mails@thomasbley.de>
2011-07-04 18:57:42 +0000

    layout news

--- src/de/tum/in/tumcampus/News.java
+++ src/de/tum/in/tumcampus/News.java
@@ -1,13 +1,55 @@
 package de.tum.in.tumcampus;
 
+import de.tum.in.tumcampus.models.NewsManager;
 import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.SimpleCursorAdapter.ViewBinder;
 
-public class News extends Activity {
+public class News extends Activity implements OnItemClickListener, ViewBinder{
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.news);
+        
+        NewsManager nm = new NewsManager(this, "database.db");
+		Cursor c = nm.getAllFromDb();
+/*
+		ListAdapter adapter = new SimpleCursorAdapter(this,
+				android.R.layout.news_listview, c, c.getColumnNames(),
+				new int[] { android.R.id.text1 });
+
+		ListView lv = (ListView) findViewById(R.id.listView);
+		lv.setAdapter(adapter);
+		lv.setOnItemClickListener(this);
+		nm.close();
+  */
     }
+
+	@Override
+	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public boolean setViewValue(View view, Cursor cursor, int index) {
+		// hide empty view elements
+		if (cursor.getString(index).length() == 0) {
+			view.setVisibility(View.GONE);
+
+			// no binding needed
+			return true;
+		} else {
+			view.setVisibility(View.VISIBLE);
+		}
+		return false;
+	}
 }
