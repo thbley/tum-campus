b4530f726329b388016b398eacf3fa0f308b3933
Christoph Richter <CeeAreCampusApp@googlemail.com>
2011-07-05 10:28:40 +0000

    added News

--- src/de/tum/in/tumcampus/News.java
+++ src/de/tum/in/tumcampus/News.java
@@ -2,7 +2,9 @@ package de.tum.in.tumcampus;
 
 import de.tum.in.tumcampus.models.NewsManager;
 import android.app.Activity;
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -21,22 +23,29 @@ public class News extends Activity implements OnItemClickListener, ViewBinder{
         
         NewsManager nm = new NewsManager(this, "database.db");
 		Cursor c = nm.getAllFromDb();
-/*
+
 		ListAdapter adapter = new SimpleCursorAdapter(this,
-				android.R.layout.news_listview, c, c.getColumnNames(),
-				new int[] { android.R.id.text1 });
+				R.layout.news_listview, c, c.getColumnNames(),
+				new int[] { R.id.image, R.id.message });
 
 		ListView lv = (ListView) findViewById(R.id.listView);
 		lv.setAdapter(adapter);
 		lv.setOnItemClickListener(this);
 		nm.close();
-  */
+  
     }
 
 	@Override
-	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+	public void onItemClick(AdapterView<?> aview, View view, int position,
+			long id) {
 		// TODO Auto-generated method stub
-		
+		ListView lv = (ListView) findViewById(R.id.listView);
+		Cursor c = (Cursor) lv.getAdapter().getItem(position);
+		String url = c.getString(c.getColumnIndex("link"));
+
+		// Connection to browser
+		Intent viewIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+		startActivity(viewIntent);
 	}
 	
 	@Override
