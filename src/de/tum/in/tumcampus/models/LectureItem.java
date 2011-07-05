8da7efea3c65cf065121c5df63f0c46bc7a77f65
Thomas Bley <thbley+github@gmail.com>
2011-07-05 06:44:20 +0000

    added lectures

---
+++ src/de/tum/in/tumcampus/models/LectureItem.java
@@ -0,0 +1,37 @@
+package de.tum.in.tumcampus.models;
+
+import java.util.Date;
+
+public class LectureItem {
+	String id;
+	String lectureId;
+	Date start;
+	Date end;
+	String name;
+	String location;
+	String note;
+	String url;
+	String seriesId;
+
+	public LectureItem(String id, String lectureId, Date start, Date end,
+			String name, String location, String note, String url,
+			String seriesId) {
+		this.id = id;
+		this.lectureId = lectureId;
+		this.start = start;
+		this.end = end;
+		this.name = name;
+		this.location = location;
+		this.note = note;
+		this.url = url;
+		this.seriesId = seriesId;
+	}
+
+	public String toString() {
+		return "id=" + id + ", lectureId=" + lectureId + ", start="
+				+ Utils.getDateTimeString(start) + ", end="
+				+ Utils.getDateTimeString(end) + ", name=" + name
+				+ ", location=" + location + ", note=" + note + ", seriesId="
+				+ seriesId + ", url=" + url;
+	}
+}
