88223f747889fe3138f7cad9e96ed581a05396df
Thomas Bley <mails@thomasbley.de>
2011-07-06 06:53:27 +0000

    added lectures

--- src/de/tum/in/tumcampus/models/LectureManager.java
+++ src/de/tum/in/tumcampus/models/LectureManager.java
@@ -1,6 +1,7 @@
 package de.tum.in.tumcampus.models;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -17,14 +18,19 @@ public class LectureManager extends SQLiteOpenHelper {
 		onCreate(db);
 	}
 
+	public Cursor getAllFromDb() {
+		return db.rawQuery("SELECT name, module, id as _id "
+				+ "FROM lectures ORDER BY name", null);
+	}
+
 	public void updateLectures() {
-		db.execSQL("REPLACE INTO lectures (id, name) "
-				+ "SELECT DISTINCT lectureId, name FROM lectures_items");
+		db.execSQL("REPLACE INTO lectures (id, name, module) "
+				+ "SELECT DISTINCT lectureId, name, module FROM lectures_items");
 	}
 
 	public void onCreate(SQLiteDatabase db) {
 		db.execSQL("CREATE TABLE IF NOT EXISTS lectures ("
-				+ "id VARCHAR PRIMARY KEY, name VARCHAR)");
+				+ "id VARCHAR PRIMARY KEY, name VARCHAR, module VARCHAR)");
 	}
 
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
