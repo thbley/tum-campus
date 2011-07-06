f3a1f265d8268f46c6980e0e885f6ed46af454a6
Thomas Bley <mails@thomasbley.de>
2011-07-06 06:44:30 +0000

    added news test

---
+++ src/de/tum/in/tumcampus/test/NewsTest.java
@@ -0,0 +1,30 @@
+package de.tum.in.tumcampus.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.jayway.android.robotium.solo.Solo;
+
+import de.tum.in.tumcampus.TumCampus;
+
+public class NewsTest extends ActivityInstrumentationTestCase2<TumCampus> {
+
+	private Solo solo; // simulates the user of the app
+
+	public NewsTest() {
+		super("de.tum.in.tumcampus", TumCampus.class);
+	}
+
+	public void setUp() {
+		solo = new Solo(getInstrumentation(), getActivity());
+	}
+
+	public void testNews() {
+		assertTrue(solo.searchText("Nachrichten"));
+
+		solo.clickOnText("Nachrichten");
+		assertTrue(solo.searchText("Testing"));
+
+		assertTrue(solo.searchText("Öffnungszeit"));
+		solo.clickOnText("Öffnungszeit");
+	}
+}
