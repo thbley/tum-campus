fd6972e4bf3c2c41d91efc45a61ca5e63a4bc32a
Thomas Bley <mails@thomasbley.de>
2011-07-06 06:41:59 +0000

    added module

--- src/de/tum/in/tumcampus/models/LectureItem.java
+++ src/de/tum/in/tumcampus/models/LectureItem.java
@@ -8,19 +8,21 @@ public class LectureItem {
 	Date start;
 	Date end;
 	String name;
+	String module;
 	String location;
 	String note;
 	String url;
 	String seriesId;
 
 	public LectureItem(String id, String lectureId, Date start, Date end,
-			String name, String location, String note, String url,
-			String seriesId) {
+			String name, String module, String location, String note,
+			String url, String seriesId) {
 		this.id = id;
 		this.lectureId = lectureId;
 		this.start = start;
 		this.end = end;
 		this.name = name;
+		this.module = module;
 		this.location = location;
 		this.note = note;
 		this.url = url;
@@ -30,8 +32,8 @@ public class LectureItem {
 	public String toString() {
 		return "id=" + id + ", lectureId=" + lectureId + ", start="
 				+ Utils.getDateTimeString(start) + ", end="
-				+ Utils.getDateTimeString(end) + ", name=" + name
-				+ ", location=" + location + ", note=" + note + ", seriesId="
-				+ seriesId + ", url=" + url;
+				+ Utils.getDateTimeString(end) + ", name=" + name + ", module="
+				+ module + ", location=" + location + ", note=" + note
+				+ ", seriesId=" + seriesId + ", url=" + url;
 	}
 }
