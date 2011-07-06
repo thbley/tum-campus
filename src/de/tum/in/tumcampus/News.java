83e40af8d976fade32cfb3a3b167d4001a16f6f6
Thomas Bley <thbley+github@gmail.com>
2011-07-06 06:55:10 +0000

    cleanup

--- src/de/tum/in/tumcampus/News.java
+++ src/de/tum/in/tumcampus/News.java
@@ -1,6 +1,5 @@
 package de.tum.in.tumcampus;
 
-import de.tum.in.tumcampus.models.NewsManager;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
@@ -8,51 +7,50 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ListAdapter;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.SimpleCursorAdapter.ViewBinder;
+import de.tum.in.tumcampus.models.NewsManager;
 
-public class News extends Activity implements OnItemClickListener, ViewBinder{
+public class News extends Activity implements OnItemClickListener, ViewBinder {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.news);
-        
-        NewsManager nm = new NewsManager(this, "database.db");
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.news);
+
+		NewsManager nm = new NewsManager(this, "database.db");
 		Cursor c = nm.getAllFromDb();
 
-		ListAdapter adapter = new SimpleCursorAdapter(this,
-				R.layout.news_listview, c, c.getColumnNames(),
-				new int[] { R.id.image, R.id.message });
+		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+				R.layout.news_listview, c, c.getColumnNames(), new int[] {
+						R.id.image, R.id.message, R.id.date });
+		adapter.setViewBinder(this);
 
 		ListView lv = (ListView) findViewById(R.id.listView);
 		lv.setAdapter(adapter);
 		lv.setOnItemClickListener(this);
 		nm.close();
-  
-    }
+	}
 
 	@Override
 	public void onItemClick(AdapterView<?> aview, View view, int position,
 			long id) {
-		// TODO Auto-generated method stub
 		ListView lv = (ListView) findViewById(R.id.listView);
 		Cursor c = (Cursor) lv.getAdapter().getItem(position);
 		String url = c.getString(c.getColumnIndex("link"));
-		
-		if (url.equals("")){
+
+		if (url.length() == 0) {
 			return;
-			// evtl. TODO: Toast "Kein Link vorhanden"
+			// TODO: Toast "Kein Link vorhanden"
 		}
 
 		// Connection to browser
 		Intent viewIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
 		startActivity(viewIntent);
 	}
-	
+
 	@Override
 	public boolean setViewValue(View view, Cursor cursor, int index) {
 		// hide empty view elements
