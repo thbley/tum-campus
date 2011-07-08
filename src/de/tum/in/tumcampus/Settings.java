a3e48ee154ef305af94950fc872a507a5727e3cf
Thomas Bley <mails@thomasbley.de>
2011-07-08 14:38:54 +0000

    added settings

---
+++ src/de/tum/in/tumcampus/Settings.java
@@ -0,0 +1,13 @@
+package de.tum.in.tumcampus;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class Settings extends PreferenceActivity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.settings);
+	}
+}
