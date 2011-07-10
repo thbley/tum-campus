c06b11b909f329872ecfd4abe72f422027e9ecec
Thomas Bley <thbley+github@gmail.com>
2011-07-10 23:58:13 +0000

    added debug flag in context menu

--- src/de/tum/in/tumcampus/test/DebugTest.java
+++ src/de/tum/in/tumcampus/test/DebugTest.java
@@ -19,19 +19,37 @@ public class DebugTest extends ActivityInstrumentationTestCase2<TumCampus> {
 	}
 
 	public void testDebug() {
+		assertFalse(solo.searchText("Debug"));
+		
+		solo.sendKey(Solo.MENU);
+		solo.clickOnText("Einstellungen");
+		solo.clickOnText("Debug-Modus");
+		solo.goBack();
+
 		assertTrue(solo.searchText("Debug"));
 		solo.clickOnText("Debug");
 
 		assertTrue(solo.searchText("Debug SQLite"));
 
+		solo.clickOnText("syncs");
 		solo.clickOnText("cafeterias");
 		solo.clickOnText("cafeterias_menus");
 		solo.clickOnText("feeds");
 		solo.clickOnText("feeds_items");
+		solo.clickOnText("lectures");
+		solo.clickOnText("lectures_items");
 		solo.clickOnText("links");
 		solo.clickOnText("events");
+		solo.clickOnText("news");
 
 		solo.goBack();
 		assertTrue(solo.searchText("Hello World"));
+		
+		solo.sendKey(Solo.MENU);
+		solo.clickOnText("Einstellungen");
+		solo.clickOnText("Debug-Modus");
+		solo.goBack();
+		
+		assertFalse(solo.searchText("Debug"));
 	}
 }
