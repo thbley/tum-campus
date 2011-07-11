cb5f8f03158cc5fe56e0c005880155239faecf37
Thomas Bley <thbley+github@gmail.com>
2011-07-11 23:40:36 +0000

    moved default datasets to assets/*.csv

--- src/de/tum/in/tumcampus/services/ImportService.java
+++ src/de/tum/in/tumcampus/services/ImportService.java
@@ -2,6 +2,7 @@ package de.tum.in.tumcampus.services;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.List;
 
 import android.app.IntentService;
 import android.app.Notification;
@@ -87,11 +88,15 @@ public class ImportService extends IntentService {
 	}
 
 	public void importTransportsDefaults() throws Exception {
-		TransportManager tm = new TransportManager(this, db);
 
+		TransportManager tm = new TransportManager(this, db);
 		if (tm.empty()) {
-			tm.replaceIntoDb("Garching-Forschungszentrum");
-			tm.replaceIntoDb("Marienplatz");
+			List<String[]> rows = Utils.readCsv(
+					getAssets().open("transports.csv"), "ISO-8859-1");
+
+			for (int i = 0; i < rows.size(); i++) {
+				tm.replaceIntoDb(rows.get(i)[0]);
+			}
 		}
 		tm.close();
 	}
@@ -104,53 +109,39 @@ public class ImportService extends IntentService {
 	}
 
 	public void importFeedsDefaults() throws Exception {
+
 		FeedManager nm = new FeedManager(this, db);
 		if (nm.empty()) {
-			nm.insertUpdateIntoDb(new Feed("Spiegel",
-					"http://www.spiegel.de/schlagzeilen/index.rss"));
+			List<String[]> rows = Utils.readCsv(getAssets().open("feeds.csv"),
+					"ISO-8859-1");
 
-			nm.insertUpdateIntoDb(new Feed("N-tv", "http://www.n-tv.de/rss"));
-
-			nm.insertUpdateIntoDb(new Feed("Zeit",
-					"http://newsfeed.zeit.de/index"));
-
-			nm.insertUpdateIntoDb(new Feed("Golem",
-					"http://rss.golem.de/rss.php?feed=RSS1.0"));
-
-			nm.insertUpdateIntoDb(new Feed("Heise",
-					"http://www.heise.de/newsticker/heise.rdf"));
-
-			nm.insertUpdateIntoDb(new Feed("MVG Störungsticker",
-					"http://www.mvg-mobil.de/Tickerrss/CreateRssClass"));
+			for (int i = 0; i < rows.size(); i++) {
+				nm.insertUpdateIntoDb(new Feed(rows.get(i)[0], rows.get(i)[1]));
+			}
 		}
 		nm.importFromInternal();
 		nm.close();
 	}
 
 	public void importLectureItemsDefaults() throws Exception {
-
-		// TODO add to resource directory
 		LectureItemManager lim = new LectureItemManager(this, db);
 		if (lim.empty()) {
-			lim.replaceIntoDb(new LectureItem.Holiday("H1", Utils
-					.getDate("2011-08-15"), "Mariä Himmelfahrt"));
+			List<String[]> rows = Utils.readCsv(
+					getAssets().open("lectures_holidays.csv"), "ISO-8859-1");
 
-			lim.replaceIntoDb(new LectureItem.Holiday("H2", Utils
-					.getDate("2011-10-03"), "Tag der Deutschen Einheit"));
+			for (int i = 0; i < rows.size(); i++) {
+				lim.replaceIntoDb(new LectureItem.Holiday(rows.get(i)[0], Utils
+						.getDate(rows.get(i)[1]), rows.get(i)[2]));
+			}
 
-			lim.replaceIntoDb(new LectureItem.Holiday("H3", Utils
-					.getDate("2011-11-01"), "Allerheiligen"));
+			rows = Utils.readCsv(getAssets().open("lectures_vacations.csv"),
+					"ISO-8859-1");
 
-			lim.replaceIntoDb(new LectureItem.Holiday("H4", Utils
-					.getDate("2011-12-08"), "Dies Academicus"));
-
-			lim.replaceIntoDb(new LectureItem.Vacation("V1", Utils
-					.getDate("2011-08-01"), Utils.getDate("2011-09-30"),
-					"Sommerferien"));
-
-			lim.replaceIntoDb(new LectureItem.Vacation("V2", Utils
-					.getDate("2011-12-24"), Utils.getDate("2012-01-06"),
-					"Weihnachtsferien"));
+			for (int i = 0; i < rows.size(); i++) {
+				lim.replaceIntoDb(new LectureItem.Vacation(rows.get(i)[0],
+						Utils.getDate(rows.get(i)[1]), Utils.getDate(rows
+								.get(i)[2]), rows.get(i)[3]));
+			}
 		}
 		lim.close();
 
@@ -172,52 +163,12 @@ public class ImportService extends IntentService {
 
 	public void importLinksDefaults() throws Exception {
 		LinkManager lm = new LinkManager(this, db);
-
 		if (lm.empty()) {
-			lm.insertUpdateIntoDb(new Link("Spiegel", "http://m.spiegel.de/"));
-
-			lm.insertUpdateIntoDb(new Link("N-tv", "http://mobil.n-tv.de/"));
-
-			lm.insertUpdateIntoDb(new Link("Zeit", "http://mobil.zeit.de/"));
-
-			lm.insertUpdateIntoDb(new Link("Golem", "http://golem.mobi/"));
-
-			lm.insertUpdateIntoDb(new Link("Heise", "http://heise-online.mobi/"));
-
-			lm.insertUpdateIntoDb(new Link("MVV EFA",
-					"http://efa.mvv-muenchen.de/mvv/XSLT_TRIP_REQUEST2?language=de"));
-
-			lm.insertUpdateIntoDb(new Link("MVG Newsticker",
-					"http://www.mvg-mobil.de/betriebsaenderungen/index.html"));
-
-			lm.insertUpdateIntoDb(new Link("MVV- und Bahn-Auskunft",
-					"http://mobile.bahn.de/bin/mobil/query2.exe/dox"));
-
-			lm.insertUpdateIntoDb(new Link(
-					"Informatik Infopoint",
-					"http://www.in.tum.de/fuer-studierende-der-tum/service-fuer-studierende/infopoint.html"));
-
-			lm.insertUpdateIntoDb(new Link("Informatik Studienberatung",
-					"http://www.in.tum.de/fuer-studierende-der-tum/beratung.html"));
-
-			lm.insertUpdateIntoDb(new Link("Stellenangebote",
-					"http://portal.mytum.de/jobs/index_html"));
-
-			lm.insertUpdateIntoDb(new Link("Fachschaft MPI",
-					"http://mpi.fs.tum.de/"));
-
-			lm.insertUpdateIntoDb(new Link("OPAC TU München",
-					"http://opac.ub.tum.de/InfoGuideClient.tumsis/start.do?Login=wotum01"));
-
-			lm.insertUpdateIntoDb(new Link(
-					"OPAC LMU",
-					"https://opacplus.ub.uni-muenchen.de/InfoGuideClient.ubmsis/start.do?Login=igubm"));
-
-			lm.insertUpdateIntoDb(new Link("Informatik Fakultät",
-					"http://www.in.tum.de/"));
-			
-			lm.insertUpdateIntoDb(new Link("Studentenwerk München",
-					"http://www.studentenwerk-muenchen.de/"));
+			List<String[]> rows = Utils.readCsv(getAssets().open("links.csv"),
+					"ISO-8859-1");
+			for (int i = 0; i < rows.size(); i++) {
+				lm.insertUpdateIntoDb(new Link(rows.get(i)[0], rows.get(i)[1]));
+			}
 		}
 		lm.close();
 	}
