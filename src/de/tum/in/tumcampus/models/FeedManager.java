262110baf4f4978fd300a63f834720c5317442ee
Thomas Bley <mails@thomasbley.de>
2011-07-12 01:26:57 +0000

    cleanup

--- src/de/tum/in/tumcampus/models/FeedManager.java
+++ src/de/tum/in/tumcampus/models/FeedManager.java
@@ -8,7 +8,6 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
 
 public class FeedManager extends SQLiteOpenHelper {
 
@@ -70,7 +69,7 @@ public class FeedManager extends SQLiteOpenHelper {
 	}
 
 	public void insertUpdateIntoDb(Feed n) throws Exception {
-		Log.d("TumCampus feeds replaceIntoDb", n.toString());
+		Utils.Log(n.toString());
 
 		if (n.name.length() == 0) {
 			throw new Exception("Invalid name.");
