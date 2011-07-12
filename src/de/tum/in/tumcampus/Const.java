2854ccfa9d9f67d76ab1ea6507af1ae2207910ec
Thomas Bley <mails@thomasbley.de>
2011-07-12 21:30:52 +0000

    cleanup

---
+++ src/de/tum/in/tumcampus/Const.java
@@ -0,0 +1,16 @@
+package de.tum.in.tumcampus;
+
+public final class Const {
+
+	public final static String db = "database.db";
+	
+	public final static int dbVersion = 1;
+
+	public static final class settings {
+		public final static String cafeteriaFilter = "cafeteriaFilter";
+
+		public final static String debug = "debug";
+
+		public final static String silence = "silence";
+	}
+}
