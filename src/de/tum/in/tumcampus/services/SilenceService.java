1c76e0f6de33fcbec392860b24ea56f5cb15a380
Thomas Bley <thbley+github@gmail.com>
2011-07-12 01:40:39 +0000

    cleanup

--- src/de/tum/in/tumcampus/services/SilenceService.java
+++ src/de/tum/in/tumcampus/services/SilenceService.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.media.AudioManager;
-import android.util.Log;
 import de.tum.in.tumcampus.models.LectureItemManager;
 import de.tum.in.tumcampus.models.Utils;
 
@@ -19,7 +18,6 @@ public class SilenceService extends IntentService {
 
 	@Override
 	protected void onHandleIntent(Intent intent) {
-
 		while (Utils.getSettingBool(this, "silence")) {
 
 			int mode = AudioManager.RINGER_MODE_NORMAL;
@@ -32,16 +30,15 @@ public class SilenceService extends IntentService {
 			c.close();
 			lim.close();
 
+			Utils.Log("set ringer mode: " + mode);
 			AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 			am.setRingerMode(mode);
-
-			Log.d("TumCampus SilenceService", "set ringer mode: " + mode);
 
 			synchronized (this) {
 				try {
 					wait(60000);
 				} catch (Exception e) {
-					Log.e("TumCampus SilenceService", e.getMessage());
+					Utils.Log(e, "");
 				}
 			}
 		}
@@ -50,14 +47,12 @@ public class SilenceService extends IntentService {
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
-
-		Log.d("TumCampus SilenceService", "TumCampus service destroy");
+		Utils.Log("");
 	}
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
-
-		Log.d("TumCampus SilenceService", "TumCampus service create");
+		Utils.Log("");
 	}
 }
