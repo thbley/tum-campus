302de03f75153cee7c3120f18a1300931d71eedd
Thomas Bley <thbley+github@gmail.com>
2011-07-13 23:23:07 +0000

    injected test data

--- src/de/tum/in/tumcampus/test/LecturesTest.java
+++ src/de/tum/in/tumcampus/test/LecturesTest.java
@@ -5,7 +5,12 @@ import android.test.ActivityInstrumentationTestCase2;
 
 import com.jayway.android.robotium.solo.Solo;
 
+import de.tum.in.tumcampus.Const;
 import de.tum.in.tumcampus.TumCampus;
+import de.tum.in.tumcampus.models.LectureItem;
+import de.tum.in.tumcampus.models.LectureItemManager;
+import de.tum.in.tumcampus.models.LectureManager;
+import de.tum.in.tumcampus.models.Utils;
 
 public class LecturesTest extends ActivityInstrumentationTestCase2<TumCampus> {
 
@@ -15,8 +20,40 @@ public class LecturesTest extends ActivityInstrumentationTestCase2<TumCampus> {
 		super("de.tum.in.tumcampus", TumCampus.class);
 	}
 
-	public void setUp() {
+	public void setUp() throws Exception {
 		solo = new Solo(getInstrumentation(), getActivity());
+
+		// inject test data
+		LectureItem li = new LectureItem("T1", "T1",
+				Utils.getDateTime("2011-05-04T14:00:00"),
+				Utils.getDateTime("2011-05-04T16:00:00"), "CSCW 2", "IN2119",
+				"01.07.023", "", "", "T1");
+
+		LectureItem li2 = new LectureItem.Holiday("TH1",
+				Utils.getDate("2011-12-13"), "Some Holiday");
+
+		LectureItemManager lim = new LectureItemManager(getActivity(), Const.db);
+		lim.replaceIntoDb(li);
+		lim.replaceIntoDb(li2);
+		lim.close();
+
+		LectureManager lm = new LectureManager(getActivity(), Const.db);
+		lm.updateLectures();
+		lm.close();
+	}
+
+	public void tearDown() throws Exception {
+		// remove test data
+		LectureItemManager lim = new LectureItemManager(
+				solo.getCurrentActivity(), Const.db);
+		lim.deleteLectureFromDb("T1");
+		lim.deleteLectureFromDb("TH1");
+		lim.close();
+
+		LectureManager lm = new LectureManager(getActivity(), Const.db);
+		lm.deleteItemFromDb("T1");
+		lm.close();
+		super.tearDown();
 	}
 
 	public void testLecturesPortrait() {
@@ -41,25 +78,36 @@ public class LecturesTest extends ActivityInstrumentationTestCase2<TumCampus> {
 
 		assertTrue(solo.searchText("Nächste Vorlesungen"));
 
-		assertTrue(solo.searchText("Logik"));
-		solo.clickOnText("Logik");
+		assertTrue(solo.searchText("CSCW"));
+		solo.clickOnText("CSCW");
+	}
+
+	public void testLecturesItemDelete() {
+		assertTrue(solo.searchText("Vorlesungen"));
+		solo.clickOnText("Vorlesungen");
+
+		solo.clickOnText("Feiertag");
+
+		assertTrue(solo.searchText("Some Holiday"));
+		solo.clickLongOnText("Some Holiday");
+
+		assertTrue(solo.searchButton("Ja"));
+		solo.clickLongOnText("Ja");
+
+		assertFalse(solo.searchText("Some Holiday"));
 	}
 
 	public void testLecturesDelete() {
 		assertTrue(solo.searchText("Vorlesungen"));
 		solo.clickOnText("Vorlesungen");
 
-		solo.clickOnText("Feiertag");
-
-		assertTrue(solo.searchText("Allerheiligen"));
-		solo.clickLongOnText("Allerheiligen");
+		assertTrue(solo.searchText("CSCW"));
+		solo.clickLongOnText("CSCW");
 
 		assertTrue(solo.searchButton("Ja"));
 		solo.clickLongOnText("Ja");
-		
-		assertFalse(solo.searchText("Allerheiligen"));
-		
-		// TODO add lecture delete
+
+		assertFalse(solo.searchText("CSCW"));
 	}
 
 	private void _testLectures() {
@@ -67,8 +115,8 @@ public class LecturesTest extends ActivityInstrumentationTestCase2<TumCampus> {
 
 		assertTrue(solo.searchText("Feiertag"));
 		solo.clickOnText("Feiertag");
-		assertTrue(solo.searchText("Academicus"));
-		assertTrue(solo.searchText("Do, 08.12.2011"));
+		assertTrue(solo.searchText("Deutschen Einheit"));
+		assertTrue(solo.searchText("Mo, 03.10.2011"));
 
 		assertTrue(solo.searchText("Ferien"));
 		solo.clickOnText("Ferien");
@@ -79,7 +127,5 @@ public class LecturesTest extends ActivityInstrumentationTestCase2<TumCampus> {
 		assertTrue(solo.searchText("Mi, 04.05.2011 14:00 - 16:00, 01.07.023"));
 		assertTrue(solo.searchText("IN2119"));
 		solo.clickOnText("IN2119");
-
-		// TODO inject test data
 	}
 }
