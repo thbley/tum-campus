4ac70cfb3133fcf4df531f4345fbff070289f267
Thomas Bley <mails@thomasbley.de>
2011-07-13 03:04:41 +0000

    cleanup

--- src/de/tum/in/tumcampus/services/ImportService.java
+++ src/de/tum/in/tumcampus/services/ImportService.java
@@ -10,6 +10,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import de.tum.in.tumcampus.Const;
 import de.tum.in.tumcampus.TumCampus;
 import de.tum.in.tumcampus.models.Feed;
 import de.tum.in.tumcampus.models.FeedManager;
@@ -26,8 +27,6 @@ public class ImportService extends IntentService {
 	public ImportService() {
 		super("ImportService");
 	}
-
-	final static String db = "database.db";
 
 	public final static String broadcast = "de.tum.in.tumcampus.intent.action.BROADCAST_IMPORT";
 
@@ -73,115 +72,126 @@ public class ImportService extends IntentService {
 				if (action.equals("lectures")) {
 					importLectureItems();
 				}
-				message("Fertig!", "completed");
 			} catch (Exception e) {
-				message(e);
+				message(e, "");
 			}
+			message("Fertig!", "completed");
 			nm.cancel(1);
 		}
 	}
 
+	public void importFeeds() {
+		FeedManager nm = new FeedManager(this, Const.db);
+		try {
+			nm.importFromInternal();
+		} catch (Exception e) {
+			message(e, nm.lastInfo);
+		}
+		nm.close();
+	}
+
+	public void importLinks() {
+		LinkManager lm = new LinkManager(this, Const.db);
+		try {
+			lm.importFromInternal();
+		} catch (Exception e) {
+			message(e, lm.lastInfo);
+		}
+		lm.close();
+	}
+
+	public void importLectureItems() {
+		LectureItemManager lim = new LectureItemManager(this, Const.db);
+		try {
+			lim.importFromInternal();
+		} catch (Exception e) {
+			message(e, lim.lastInfo);
+		}
+		lim.close();
+
+		LectureManager lm = new LectureManager(this, Const.db);
+		lm.updateLectures();
+		lm.close();
+	}
+
 	public void importTransportsDefaults() throws Exception {
 
-		TransportManager tm = new TransportManager(this, db);
+		TransportManager tm = new TransportManager(this, Const.db);
 		if (tm.empty()) {
 			List<String[]> rows = Utils.readCsv(
 					getAssets().open("transports.csv"), "ISO-8859-1");
 
-			for (int i = 0; i < rows.size(); i++) {
-				tm.replaceIntoDb(rows.get(i)[0]);
+			for (String[] row : rows) {
+				tm.replaceIntoDb(row[0]);
 			}
 		}
 		tm.close();
 	}
 
-	public void importFeeds() throws Exception {
-		FeedManager nm = new FeedManager(this, db);
-		nm.importFromInternal();
-		nm.close();
-	}
-
 	public void importFeedsDefaults() throws Exception {
 
-		FeedManager nm = new FeedManager(this, db);
+		FeedManager nm = new FeedManager(this, Const.db);
 		if (nm.empty()) {
 			List<String[]> rows = Utils.readCsv(getAssets().open("feeds.csv"),
 					"ISO-8859-1");
 
-			for (int i = 0; i < rows.size(); i++) {
-				nm.insertUpdateIntoDb(new Feed(rows.get(i)[0], rows.get(i)[1]));
+			for (String[] row : rows) {
+				nm.insertUpdateIntoDb(new Feed(row[0], row[1]));
 			}
 		}
-		nm.importFromInternal();
 		nm.close();
 	}
 
 	public void importLectureItemsDefaults() throws Exception {
-		LectureItemManager lim = new LectureItemManager(this, db);
+		LectureItemManager lim = new LectureItemManager(this, Const.db);
 		if (lim.empty()) {
 			List<String[]> rows = Utils.readCsv(
 					getAssets().open("lectures_holidays.csv"), "ISO-8859-1");
 
-			for (int i = 0; i < rows.size(); i++) {
-				lim.replaceIntoDb(new LectureItem.Holiday(rows.get(i)[0], Utils
-						.getDate(rows.get(i)[1]), rows.get(i)[2]));
+			for (String[] row : rows) {
+				lim.replaceIntoDb(new LectureItem.Holiday(row[0], Utils
+						.getDate(row[1]), row[2]));
 			}
 
 			rows = Utils.readCsv(getAssets().open("lectures_vacations.csv"),
 					"ISO-8859-1");
 
-			for (int i = 0; i < rows.size(); i++) {
-				lim.replaceIntoDb(new LectureItem.Vacation(rows.get(i)[0],
-						Utils.getDate(rows.get(i)[1]), Utils.getDate(rows
-								.get(i)[2]), rows.get(i)[3]));
+			for (String[] row : rows) {
+				lim.replaceIntoDb(new LectureItem.Vacation(row[0], Utils
+						.getDate(row[1]), Utils.getDate(row[2]), row[3]));
 			}
 		}
 		lim.close();
 
-		LectureManager lm = new LectureManager(this, db);
-		lm.updateLectures();
-		lm.close();
-	}
-
-	public void importLectureItems() throws Exception {
-		LectureItemManager lim = new LectureItemManager(this, db);
-		lim.importFromInternal();
-		lim.close();
-
-		LectureManager lm = new LectureManager(this, db);
+		LectureManager lm = new LectureManager(this, Const.db);
 		lm.updateLectures();
 		lm.close();
 	}
 
 	public void importLinksDefaults() throws Exception {
-		LinkManager lm = new LinkManager(this, db);
+		LinkManager lm = new LinkManager(this, Const.db);
 		if (lm.empty()) {
 			List<String[]> rows = Utils.readCsv(getAssets().open("links.csv"),
 					"ISO-8859-1");
-			for (int i = 0; i < rows.size(); i++) {
-				lm.insertUpdateIntoDb(new Link(rows.get(i)[0], rows.get(i)[1]));
+
+			for (String[] row : rows) {
+				lm.insertUpdateIntoDb(new Link(row[0], row[1]));
 			}
 		}
 		lm.close();
 	}
 
-	public void importLinks() throws Exception {
-		LinkManager lm = new LinkManager(this, db);
-		lm.importFromInternal();
-		lm.close();
-	}
-
-	public void message(Exception e) {
-		Utils.Log(e, "");
+	public void message(Exception e, String info) {
+		Utils.Log(e, info);
 
 		StringWriter sw = new StringWriter();
 		e.printStackTrace(new PrintWriter(sw));
 
 		String message = e.getMessage();
-		if (Utils.getSettingBool(this, "debug")) {
+		if (Utils.getSettingBool(this, Const.settings.debug)) {
 			message += sw.toString();
 		}
-		message(message, "error");
+		message(info + " " + message, "error");
 	}
 
 	public void message(String message, String action) {
