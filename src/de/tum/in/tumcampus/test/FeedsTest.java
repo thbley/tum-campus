87e94242e9a952f9866a1f1a6cf59de2c273ae18
Thomas Bley <mails@thomasbley.de>
2011-07-14 05:54:43 +0000

    cleanup

--- src/de/tum/in/tumcampus/test/FeedsTest.java
+++ src/de/tum/in/tumcampus/test/FeedsTest.java
@@ -3,39 +3,76 @@ package de.tum.in.tumcampus.test;
 import java.util.Date;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.ListView;
 
 import com.jayway.android.robotium.solo.Solo;
 
+import de.tum.in.tumcampus.Const;
 import de.tum.in.tumcampus.TumCampus;
+import de.tum.in.tumcampus.models.Feed;
+import de.tum.in.tumcampus.models.FeedItem;
+import de.tum.in.tumcampus.models.FeedItemManager;
+import de.tum.in.tumcampus.models.FeedManager;
+import de.tum.in.tumcampus.models.Utils;
 
 public class FeedsTest extends ActivityInstrumentationTestCase2<TumCampus> {
 
 	private Solo solo; // simulates the user of the app
 
+	private int feedId;
+
 	public FeedsTest() {
 		super("de.tum.in.tumcampus", TumCampus.class);
 	}
 
-	public void setUp() {
+	public void setUp() throws Exception {
 		solo = new Solo(getInstrumentation(), getActivity());
+
+		FeedManager fm = new FeedManager(getActivity(), Const.db);
+		feedId = fm.insertUpdateIntoDb(new Feed("Test feed",
+				"http://www.test.de"));
+		fm.close();
+
+		// inject test data
+		FeedItem fi = new FeedItem(feedId, "Test message",
+				"http://www.test.de", "Test description",
+				Utils.getDateTime("2011-05-04T14:00:00"), "");
+
+		FeedItemManager fim = new FeedItemManager(getActivity(), Const.db);
+		fim.insertIntoDb(fi);
+		fim.close();
 	}
 
-	public void testFeedsList() {
+	public void tearDown() throws Exception {
+		// remove test data
+		FeedItemManager fim = new FeedItemManager(getActivity(), Const.db);
+		fim.removeCache();
+		fim.close();
+
+		FeedManager fm = new FeedManager(getActivity(), Const.db);
+		fm.deleteFromDb(feedId);
+		fm.close();
+		super.tearDown();
+	}
+
+	private void testFeedsList() {
 		assertTrue(solo.searchText("RSS-Feeds"));
 		solo.clickOnText("RSS-Feeds");
 
 		assertTrue(solo.searchText("Feed auswählen"));
 
-		assertTrue(solo.searchText("Spiegel"));
-		solo.clickOnText("Spiegel");
+		assertTrue(solo.searchText("Test feed"));
+		solo.clickOnText("Test feed");
 
-		assertTrue(solo.searchText("Nachrichten: Spiegel"));
+		assertTrue(solo.searchText("Nachrichten: Test feed"));
+		assertTrue(solo.searchText("Test message"));
+		assertTrue(solo.searchText("Test description"));
 
 		solo.goBack();
 		assertTrue(solo.searchText("Hello World"));
 	}
 
-	public void testFeedsContextMenu() {
+	private void testFeedsContextMenu() {
 		assertTrue(solo.searchText("RSS-Feeds"));
 		solo.clickOnText("RSS-Feeds");
 
@@ -52,13 +89,15 @@ public class FeedsTest extends ActivityInstrumentationTestCase2<TumCampus> {
 		solo.sleep(2000);
 	}
 
-	public void testAFeedsCreateDelete() {
+	public void testFeedsCreateDelete() {
 		assertTrue(solo.searchText("RSS-Feeds"));
 		solo.clickOnText("RSS-Feeds");
 
 		assertTrue(solo.searchText("Feed auswählen"));
 
-		solo.clickOnEditText(0);
+		// scrollDown not working here
+		solo.drag(200, 200, 600, 200, 40);
+
 		String name = "some name " + new Date();
 		solo.enterText(0, "http://www.heise.de");
 		solo.enterText(1, name);
