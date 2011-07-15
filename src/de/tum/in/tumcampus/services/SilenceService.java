60a30edf93d1beb15e4188a002fbe3768e6f3818
Thomas Bley <mails@thomasbley.de>
2011-07-15 03:08:45 +0000

    added docu

--- src/de/tum/in/tumcampus/services/SilenceService.java
+++ src/de/tum/in/tumcampus/services/SilenceService.java
@@ -9,8 +9,14 @@ import de.tum.in.tumcampus.Const;
 import de.tum.in.tumcampus.models.LectureItemManager;
 import de.tum.in.tumcampus.models.Utils;
 
+/**
+ * Service used to silence the mobile during lectures
+ */
 public class SilenceService extends IntentService {
 
+	/**
+	 * interval in milli seconds to check for current lectures 
+	 */
 	public static int interval = 60000;
 	
 	public SilenceService() {
@@ -19,22 +25,28 @@ public class SilenceService extends IntentService {
 
 	@Override
 	protected void onHandleIntent(Intent intent) {
+		
+		// loop until silence mode gets disabled in settings
 		while (Utils.getSettingBool(this, Const.settings.silence)) {
 
+			// default: no silence
 			int mode = AudioManager.RINGER_MODE_NORMAL;
 
 			LectureItemManager lim = new LectureItemManager(this, Const.db);
 			Cursor c = lim.getCurrentFromDb();
 			if (c.getCount() != 0) {
+				// if current lecture(s) found, silence the mobile 
 				mode = AudioManager.RINGER_MODE_SILENT;
 			}
 			c.close();
 			lim.close();
 
 			Utils.Log("set ringer mode: " + mode);
+			// execute (no-)silence mode
 			AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 			am.setRingerMode(mode);
 
+			// wait unteil next check
 			synchronized (this) {
 				try {
 					wait(interval);
@@ -48,12 +60,12 @@ public class SilenceService extends IntentService {
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
-		Utils.Log("");
+		Utils.Log(""); // log destroy
 	}
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		Utils.Log("");
+		Utils.Log(""); // log create
 	}
 }
