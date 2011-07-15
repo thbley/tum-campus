b541aee39cc7c77aec9a1ca3b1454d96cab409f3
Thomas Bley <thbley+github@gmail.com>
2011-07-15 03:08:14 +0000

    added docu

--- src/de/tum/in/tumcampus/receivers/BootCompleted.java
+++ src/de/tum/in/tumcampus/receivers/BootCompleted.java
@@ -5,11 +5,15 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+/**
+ * Receiver used to start the silenceService after the boot
+ */
 public class BootCompleted extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 
+		// handle only boot completed
 		if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
 
 			Intent i = new Intent(context, SilenceService.class);
