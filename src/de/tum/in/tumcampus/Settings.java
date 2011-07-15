a644745c87596c2138bc1d526813b36c0f50e41f
Thomas Bley <mails@thomasbley.de>
2011-07-15 03:06:50 +0000

    added docu

--- src/de/tum/in/tumcampus/Settings.java
+++ src/de/tum/in/tumcampus/Settings.java
@@ -3,6 +3,9 @@ package de.tum.in.tumcampus;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
+/**
+ * Activity to show the settings dialog
+ */
 public class Settings extends PreferenceActivity {
 
 	@Override
