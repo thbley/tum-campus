093482eb0289ef8d319c94c8cabbf6d68e34db70
Thomas Bley <thbley+github@gmail.com>
2011-07-16 16:51:13 +0000

    added docu

--- src/de/tum/in/tumcampus/Lectures.java
+++ src/de/tum/in/tumcampus/Lectures.java
@@ -21,9 +21,15 @@ import de.tum.in.tumcampus.models.LectureItemManager;
 import de.tum.in.tumcampus.models.LectureManager;
 import de.tum.in.tumcampus.models.Utils;
 
+/**
+ * Activity to show lectures and lecture units
+ */
 public class Lectures extends Activity implements OnItemClickListener,
 		OnItemLongClickListener, ViewBinder {
 
+	/**
+	 * Current lecture selected
+	 */
 	String lectureId;
 
 	@Override
@@ -36,6 +42,7 @@ public class Lectures extends Activity implements OnItemClickListener,
 			setContentView(R.layout.lectures);
 		}
 
+		// get all upcoming lecture units
 		LectureItemManager lim = new LectureItemManager(this, Const.db);
 		Cursor c = lim.getRecentFromDb();
 
@@ -50,6 +57,7 @@ public class Lectures extends Activity implements OnItemClickListener,
 		lv.setOnItemLongClickListener(this);
 		lim.close();
 
+		// get all lectures
 		LectureManager lm = new LectureManager(this, Const.db);
 		Cursor c2 = lm.getAllFromDb();
 
@@ -60,6 +68,7 @@ public class Lectures extends Activity implements OnItemClickListener,
 
 			@Override
 			public boolean setViewValue(View view, Cursor c, int index) {
+				// truncate lecture names to 20 characters
 				String name = c.getString(index);
 				TextView tv = (TextView) view;
 				tv.setText(Utils.trunc(name, 20));
@@ -73,14 +82,20 @@ public class Lectures extends Activity implements OnItemClickListener,
 		lv2.setOnItemLongClickListener(this);
 		lm.close();
 
+		// reset new items counter
 		LectureItemManager.lastInserted = 0;
 	}
 
 	@Override
+	/**
+	 * change presentation of lecture units in the list
+	 */
 	public boolean setViewValue(View view, Cursor c, int index) {
 		String[] weekDays = "So,Mo,Di,Mi,Do,Fr,Sa".split(",");
 
 		if (view.getId() == android.R.id.text1) {
+			// truncate lecture name to 20 characters,
+			// append lecture unit note
 			String name = c.getString(c.getColumnIndex("name"));
 			String note = c.getString(c.getColumnIndex("note"));
 			if (note.length() > 0) {
@@ -91,6 +106,16 @@ public class Lectures extends Activity implements OnItemClickListener,
 			return true;
 		}
 		if (view.getId() == android.R.id.text2) {
+			/**
+			 * <pre>
+			 * show info as:
+			 * Lecture: Week-day, Start DateTime - End Time, Room-Nr-Intern
+			 * Holiday: Week-day, Start Date
+			 * vacation info: Start Date - End Date
+			 * 
+			 * Location format: Room-Nr-Intern, Room-name (Room-Nr-Extern)
+			 * </pre>
+			 */
 			String info = "";
 			String lectureId = c.getString(c.getColumnIndex("lectureId"));
 
@@ -128,12 +153,14 @@ public class Lectures extends Activity implements OnItemClickListener,
 		ListView lv = (ListView) findViewById(R.id.listView);
 		ListView lv2 = (ListView) findViewById(R.id.listView2);
 
+		// Click on lecture list
 		if (av.getId() == R.id.listView2) {
 			Cursor c2 = (Cursor) lv2.getAdapter().getItem(position);
 			lectureId = c2.getString(c2.getColumnIndex("_id"));
 			String name = c2.getString(c2.getColumnIndex("name"));
 			String module = c2.getString(c2.getColumnIndex("module"));
 
+			// get all lecture units from a lecture
 			LectureItemManager lim = new LectureItemManager(this, Const.db);
 
 			SimpleCursorAdapter adapter = (SimpleCursorAdapter) lv.getAdapter();
@@ -143,6 +170,7 @@ public class Lectures extends Activity implements OnItemClickListener,
 			TextView tv = (TextView) findViewById(R.id.lectureText);
 			tv.setText(Utils.trunc(name + ":", 35));
 
+			// Link to lecture module homepage (e.g. contains ECTS)
 			String moduleUrl = "https://drehscheibe.in.tum.de/myintum/kurs_verwaltung/cm.html.de?id="
 					+ module;
 
@@ -153,12 +181,16 @@ public class Lectures extends Activity implements OnItemClickListener,
 			return;
 		}
 
+		// click on lecture unit list
 		Cursor c = (Cursor) lv.getAdapter().getItem(position);
 		String url = c.getString(c.getColumnIndex("url"));
 
+		// empty link => no action
 		if (url.equals("about:blank")) {
 			return;
 		}
+
+		// tumonline search page => more lecture details
 		if (url.length() == 0) {
 			url = "https://campus.tum.de/tumonline/wbSuche.LVSucheSimple?"
 					+ "pLVNrFlag=J&pSjNr=1573&pSemester=A&pSuchbegriff="
@@ -170,6 +202,13 @@ public class Lectures extends Activity implements OnItemClickListener,
 		startActivity(viewIntent);
 	}
 
+	/**
+	 * Deletes a lecture unit and refreshes the lecture unit list
+	 * 
+	 * <pre>
+	 * @param itemId Lecture unit id
+	 * </pre>
+	 */
 	public void deleteLectureItem(String itemId) {
 		// delete lecture item
 		LectureItemManager lim = new LectureItemManager(this, Const.db);
@@ -185,11 +224,19 @@ public class Lectures extends Activity implements OnItemClickListener,
 		lim.close();
 	}
 
+	/**
+	 * Deletes a lecture and refreshes both list views
+	 * 
+	 * <pre>
+	 * @param itemId Lecture id
+	 * </pre>
+	 */
 	public void deleteLecture(String itemId) {
 		// delete lecture
 		LectureManager lm = new LectureManager(this, Const.db);
 		lm.deleteItemFromDb(itemId);
 
+		// refresh lecture list
 		ListView lv2 = (ListView) findViewById(R.id.listView2);
 		SimpleCursorAdapter adapter = (SimpleCursorAdapter) lv2.getAdapter();
 		adapter.changeCursor(lm.getAllFromDb());
@@ -199,8 +246,11 @@ public class Lectures extends Activity implements OnItemClickListener,
 		LectureItemManager lim = new LectureItemManager(this, Const.db);
 		lim.deleteLectureFromDb(itemId);
 
+		// refresh lecture unit list if viewing deleted lecture or
+		// recent lectures (could contain a unit from deleted lecture)
 		ListView lv = (ListView) findViewById(R.id.listView);
 		adapter = (SimpleCursorAdapter) lv.getAdapter();
+
 		if (lectureId == null || lectureId.equals(itemId)) {
 			adapter.changeCursor(lim.getRecentFromDb());
 
@@ -210,6 +260,7 @@ public class Lectures extends Activity implements OnItemClickListener,
 			TextView tv2 = (TextView) findViewById(R.id.moduleText);
 			tv2.setText("");
 
+			// unselect current lecture (no longer exists)
 			lectureId = null;
 		}
 		lim.close();
@@ -219,6 +270,7 @@ public class Lectures extends Activity implements OnItemClickListener,
 	public boolean onItemLongClick(final AdapterView<?> av, View v,
 			final int position, long id) {
 
+		// confirm deleting lectures or lecture units
 		DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
 			public void onClick(DialogInterface dialog, int id) {
 
