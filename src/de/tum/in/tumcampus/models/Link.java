94c46e99a8907495f58fd0a7ddc3de0ff3b636fa
Thomas Bley <thbley+github@gmail.com>
2011-07-17 17:24:53 +0000

    added docu

--- src/de/tum/in/tumcampus/models/Link.java
+++ src/de/tum/in/tumcampus/models/Link.java
@@ -1,9 +1,27 @@
 ﻿package de.tum.in.tumcampus.models;
 
+/**
+ * Link object
+ */
 public class Link {
+	/**
+	 * Name, e.g. TUM
+	 */
 	String name;
+
+	/**
+	 * Url, e.g. http://www.in.tum.de
+	 */
 	String url;
 
+	/**
+	 * New Link
+	 * 
+	 * <pre>
+	 * @param name Name, e.g. TUM
+	 * @param url Url, e.g. http://www.in.tum.de
+	 * </pre>
+	 */
 	public Link(String name, String url) {
 		this.name = name;
 		this.url = url;
