b4118c6e3e577353f3ee0d90704ab35d7579687c
Thomas Bley <mails@thomasbley.de>
2011-07-17 22:29:42 +0000

    added docu

--- src/de/tum/in/tumcampus/models/FeedItem.java
+++ src/de/tum/in/tumcampus/models/FeedItem.java
@@ -2,15 +2,53 @@
 
 import java.util.Date;
 
+/**
+ * FeedItem object
+ */
 public class FeedItem {
 
+	/**
+	 * Feed ID
+	 */
 	int feedId;
+
+	/**
+	 * Item title
+	 */
 	String title;
+
+	/**
+	 * Item link, e.g. http://www.heise.de/...
+	 */
 	String link;
+
+	/**
+	 * description Item description
+	 */
 	String description;
+
+	/**
+	 * DateTime
+	 */
 	Date date;
+
+	/**
+	 * image Local image, e.g. /mnt/sdcard/tumcampus/feeds/cache/xy.jpg
+	 */
 	String image;
 
+	/**
+	 * New Feed item (news)
+	 * 
+	 * <pre>
+	 * @param feedId Feed ID
+	 * @param title Item title
+	 * @param link Item link, e.g. http://www.heise.de/...
+	 * @param description Item description
+	 * @param date DateTime
+	 * @param image Local image, e.g. /mnt/sdcard/tumcampus/feeds/cache/xy.jpg
+	 * </pre>
+	 */
 	public FeedItem(int feedId, String title, String link, String description,
 			Date date, String image) {
 
