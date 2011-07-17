b942c4ea140a10114d04b107be9793eb4f2eba32
Thomas Bley <mails@thomasbley.de>
2011-07-17 17:20:23 +0000

    added docu

--- src/de/tum/in/tumcampus/models/News.java
+++ src/de/tum/in/tumcampus/models/News.java
@@ -2,6 +2,9 @@
 
 import java.util.Date;
 
+/**
+ * News Object
+ */
 public class News {
 
 	String id;
@@ -10,6 +13,17 @@ public class News {
 	String image;
 	Date date;
 
+	/**
+	 * New News
+	 * 
+	 * <pre>
+	 * @param id News Facebook-ID
+	 * @param message News message, e.g. X released
+	 * @param link Url, e.g. http://www.in.tum.de
+	 * @param image Local image, e.g. /mnt/sdcard/tumcampus/news/cache/xy.jpg
+	 * @param date News date
+	 * </pre>
+	 */
 	public News(String id, String message, String link, String image, Date date) {
 
 		this.id = id;
