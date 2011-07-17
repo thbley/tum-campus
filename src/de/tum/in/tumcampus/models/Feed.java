56b65c21a652945658fa875c82a6b47fec5a3140
Thomas Bley <mails@thomasbley.de>
2011-07-17 17:24:17 +0000

    added docu

--- src/de/tum/in/tumcampus/models/Feed.java
+++ src/de/tum/in/tumcampus/models/Feed.java
@@ -1,9 +1,28 @@
 ﻿package de.tum.in.tumcampus.models;
 
+/**
+ * Feed object
+ */
 public class Feed {
+
+	/**
+	 * Feed name, e.g. Spiegel
+	 */
 	String name;
+
+	/**
+	 * Feed Url, e.g. http://www.spiegel.de/schlagzeilen/index.rss
+	 */
 	String feedUrl;
 
+	/**
+	 * New Feed
+	 * 
+	 * <pre>
+	 * @param name Feed name, e.g. Spiegel
+	 * @param feedUrl Feed Url, e.g. http://www.spiegel.de/schlagzeilen/index.rss
+	 * </pre>
+	 */
 	public Feed(String name, String feedUrl) {
 		this.name = name;
 		this.feedUrl = feedUrl;
