81ecd57db529247bc70d78f0fa872be8e6a37d69
Thomas Bley <mails@thomasbley.de>
2011-07-17 17:43:50 +0000

    added docu

--- src/de/tum/in/tumcampus/models/Cafeteria.java
+++ src/de/tum/in/tumcampus/models/Cafeteria.java
@@ -1,10 +1,33 @@
 ﻿package de.tum.in.tumcampus.models;
 
+/**
+ * Cafeteria Object
+ */
 public class Cafeteria {
+	/**
+	 * Cafeteria ID, e.g. 412
+	 */
 	int id;
+
+	/**
+	 * Name, e.g. MensaX
+	 */
 	String name;
+
+	/**
+	 * Address, e.g. Boltzmannstr. 3
+	 */
 	String address;
 
+	/**
+	 * new Cafeteria
+	 * 
+	 * <pre>
+	 * @param id Cafeteria ID, e.g. 412
+	 * @param name Name, e.g. MensaX
+	 * @param address Address, e.g. Boltzmannstr. 3
+	 * </pre>
+	 */
 	public Cafeteria(int id, String name, String address) {
 		this.id = id;
 		this.name = name;
