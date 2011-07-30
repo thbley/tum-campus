e33286232fa20790d78142aee55496bd4128420f
Thomas Bley <thbley+github@gmail.com>
2011-07-30 11:56:03 +0200

    update

--- gen/de/tum/in/tumcampus/R.java
+++ gen/de/tum/in/tumcampus/R.java
@@ -13,14 +13,15 @@ public final class R {
     public static final class drawable {
         public static final int essen=0x7f020000;
         public static final int globus=0x7f020001;
-        public static final int icon=0x7f020002;
-        public static final int info=0x7f020003;
-        public static final int party=0x7f020004;
-        public static final int rss=0x7f020005;
-        public static final int tum_logo=0x7f020006;
-        public static final int vorlesung=0x7f020007;
-        public static final int www=0x7f020008;
-        public static final int zug=0x7f020009;
+        public static final int ic_menu_refresh=0x7f020002;
+        public static final int icon=0x7f020003;
+        public static final int info=0x7f020004;
+        public static final int party=0x7f020005;
+        public static final int rss=0x7f020006;
+        public static final int tum_logo=0x7f020007;
+        public static final int vorlesung=0x7f020008;
+        public static final int www=0x7f020009;
+        public static final int zug=0x7f02000a;
     }
     public static final class id {
         public static final int cafeteriaText=0x7f060000;
@@ -54,19 +55,21 @@ public final class R {
         public static final int listView2=0x7f060002;
         public static final int listView3=0x7f060001;
         public static final int lname=0x7f06001e;
+        public static final int map=0x7f06002d;
         public static final int menu=0x7f060022;
         public static final int message=0x7f060027;
         public static final int moduleText=0x7f06001d;
         public static final int name=0x7f060018;
         public static final int refresh=0x7f06001f;
         public static final int save=0x7f06001a;
-        public static final int search=0x7f06002b;
+        public static final int search=0x7f06002c;
         public static final int slide=0x7f060005;
         public static final int slider=0x7f060003;
         public static final int title=0x7f06001b;
-        public static final int transportText=0x7f060029;
-        public static final int transportText2=0x7f06002a;
+        public static final int transportText=0x7f06002a;
+        public static final int transportText2=0x7f06002b;
         public static final int url=0x7f060019;
+        public static final int webView=0x7f060029;
     }
     public static final class layout {
         public static final int appinfo=0x7f030000;
@@ -89,8 +92,9 @@ public final class R {
         public static final int main_listview=0x7f030011;
         public static final int news=0x7f030012;
         public static final int news_listview=0x7f030013;
-        public static final int transports=0x7f030014;
-        public static final int transports_horizontal=0x7f030015;
+        public static final int plans=0x7f030014;
+        public static final int transports=0x7f030015;
+        public static final int transports_horizontal=0x7f030016;
     }
     public static final class string {
         public static final int hello=0x7f050000;
