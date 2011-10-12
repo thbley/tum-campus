0da3298b8fef8c3e766c7f2268d38e9eaebcf98b
Thomas Bley <thbley+github@gmail.com>
2011-10-12 07:59:01 +0200

    removed hours, docu

---
+++ src/de/tum/in/tumcampus/models/LocationManager.java
@@ -0,0 +1,102 @@
+package de.tum.in.tumcampus.models;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import de.tum.in.tumcampus.Const;
+
+/**
+ * Location manager, handles database stuff
+ */
+public class LocationManager extends SQLiteOpenHelper {
+
+	/**
+	 * Database connection
+	 */
+	private SQLiteDatabase db;
+
+	/**
+	 * Constructor, open/create database, create table if necessary
+	 * 
+	 * <pre>
+	 * @param context Context
+	 * @param database Filename, e.g. database.db
+	 * </pre>
+	 */
+	public LocationManager(Context context, String database) {
+		super(context, database, null, Const.dbVersion);
+
+		db = getWritableDatabase();
+		onCreate(db);
+	}
+
+	/**
+	 * Get all locations by category from the database
+	 * 
+	 * <pre>
+	 * @param category String Location category, e.g. library, cafeteria
+	 * @return Database cursor (name, address, transport, hours, remark, 
+	 * 		url, _id)
+	 * </pre>
+	 */
+	public Cursor getAllHoursFromDb(String category) {
+		return db.rawQuery("SELECT name, address, transport, hours, "
+				+ "remark, url, id as _id "
+				+ "FROM locations WHERE category=? ORDER BY name",
+				new String[] { category });
+	}
+
+	/**
+	 * Checks if the locations table is empty
+	 * 
+	 * @return true if no locations are available, else false
+	 */
+	public boolean empty() {
+		boolean result = true;
+		Cursor c = db.rawQuery("SELECT id FROM locations LIMIT 1", null);
+		if (c.moveToNext()) {
+			result = false;
+		}
+		c.close();
+		return result;
+	}
+
+	/**
+	 * Replaces a location in the database
+	 * 
+	 * <pre>
+	 * @param l Location object
+	 * @throws Exception
+	 * </pre>
+	 */
+	public void replaceIntoDb(Location l) throws Exception {
+		Utils.log(l.toString());
+
+		if (l.id <= 0) {
+			throw new Exception("Invalid id.");
+		}
+		if (l.name.length() == 0) {
+			throw new Exception("Invalid name.");
+		}
+		db.execSQL("REPLACE INTO locations (id, category, name, address, "
+				+ "transport, hours, remark, url) VALUES "
+				+ "(?, ?, ?, ?, ?, ?, ?, ?)",
+				new String[] { String.valueOf(l.id), l.category, l.name,
+						l.address, l.transport, l.hours, l.remark, l.url });
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// create table if needed
+		db.execSQL("CREATE TABLE IF NOT EXISTS locations ("
+				+ "id INTEGER PRIMARY KEY, category VARCHAR, "
+				+ "name VARCHAR, address VARCHAR, transport VARCHAR, "
+				+ "hours VARCHAR, remark VARCHAR, url VARCHAR)");
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		onCreate(db);
+	}
+}
