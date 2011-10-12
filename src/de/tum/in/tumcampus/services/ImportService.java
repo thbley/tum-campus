7fab62b4e5f48e657ff3a5981bf0165d88e51233
Thomas Bley <mails@thomasbley.de>
2011-10-12 07:57:40 +0200

    dropped hours2

--- src/de/tum/in/tumcampus/services/ImportService.java
+++ src/de/tum/in/tumcampus/services/ImportService.java
@@ -189,7 +189,7 @@ public class ImportService extends IntentService {
 
 			for (String[] row : rows) {
 				lm.replaceIntoDb(new Location(Integer.parseInt(row[0]), row[1],
-						row[2], row[3], row[4], row[5], row[6], row[7], row[8]));
+						row[2], row[3], row[4], row[5], row[6], row[7]));
 			}
 		}
 		lm.close();
