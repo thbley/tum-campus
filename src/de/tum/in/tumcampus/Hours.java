c048532be10d898f014fd5f7af80467eda03bff0
Thomas Bley <mails@thomasbley.de>
2011-10-13 06:08:38 +0200

    added room field, cleanup

--- src/de/tum/in/tumcampus/Hours.java
+++ src/de/tum/in/tumcampus/Hours.java
@@ -1,8 +1,11 @@
 package de.tum.in.tumcampus;
 
+import java.util.regex.Pattern;
+
 import android.app.Activity;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.text.util.Linkify;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -19,13 +22,13 @@ import de.tum.in.tumcampus.models.LocationManager;
  */
 public class Hours extends Activity implements OnItemClickListener, ViewBinder {
 
-	private String[] names = new String[] { "Bibliotheken", "Mensen",
-			"Information" };
+	private String[] names = new String[] { "Bibliotheken", "Information",
+			"Mensa Garching", "Mensa Großhadern", "Mensa Innenstadt",
+			"Mensa Pasing", "Mensa Weihenstephan" };
 
-	// TODO add test cases
-
-	// TODO optimize categories
-	private String[] categories = new String[] { "library", "cafeteria", "info" };
+	private String[] categories = new String[] { "library", "info",
+			"cafeteria_gar", "cafeteria_grh", "cafeteria", "cafeteria_pas",
+			"cafeteria_wst" };
 
 	private static int position = -1;
 
@@ -50,7 +53,7 @@ public class Hours extends Activity implements OnItemClickListener, ViewBinder {
 	protected void onResume() {
 		super.onResume();
 
-		// refresh current selected plan on resume (rotate)
+		// refresh current selected category on resume (rotate)
 		if (position != -1) {
 			onItemClick(null, null, position, 0);
 		}
@@ -60,23 +63,10 @@ public class Hours extends Activity implements OnItemClickListener, ViewBinder {
 	public void onItemClick(AdapterView<?> av, View v, int pos, long id) {
 		position = pos;
 
-		// click on feed item in list, open URL in browser
-		// if (av.getId() == R.id.listView2) {
-		// TODO implement URL?
-		/*
-		 * Cursor c = (Cursor) av.getAdapter().getItem(position); String link =
-		 * c.getString(c.getColumnIndex("link"));
-		 * 
-		 * Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
-		 * startActivity(intent); return;
-		 */
-		// }
-
 		SlidingDrawer sd = (SlidingDrawer) findViewById(R.id.slider);
 		if (sd.isOpened()) {
 			sd.animateClose();
 		}
-
 		setTitle("Öffnungszeiten: " + names[position]);
 
 		// click on category in list
@@ -85,24 +75,26 @@ public class Hours extends Activity implements OnItemClickListener, ViewBinder {
 
 		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
 				android.R.layout.two_line_list_item, c, c.getColumnNames(),
-				new int[] { android.R.id.text1, android.R.id.text2 });
+				new int[] { android.R.id.text1, android.R.id.text2 }) {
+
+			@Override
+			public boolean isEnabled(int position) {
+				// disable onclick
+				return false;
+			}
+		};
 		adapter.setViewBinder(this);
 
 		ListView lv2 = (ListView) findViewById(R.id.listView2);
 		lv2.setAdapter(adapter);
-		// lv2.setOnItemClickListener(this);
 		lm.close();
-
-		// TODO cleanup
-
-		// TODO documentation
 
 		// TODO add hours to cafeteria
 	}
 
 	@Override
 	/**
-	 * change presentation of lecture units in the list
+	 * change presentation of locations in the list
 	 */
 	public boolean setViewValue(View view, Cursor c, int index) {
 		if (view.getId() == android.R.id.text2) {
@@ -111,19 +103,25 @@ public class Hours extends Activity implements OnItemClickListener, ViewBinder {
 
 			String hours = c.getString(c.getColumnIndex("hours"));
 			String remark = c.getString(c.getColumnIndex("remark"));
+			String room = c.getString(c.getColumnIndex("room"));
 
-			// TODO use stringbuilder
-			// TODO put room into extra field
-
-			String content = hours + "\n" + address;
+			StringBuilder sb = new StringBuilder(hours + "\n" + address);
+			if (room.length() > 0) {
+				sb.append(", " + room);
+			}
 			if (transport.length() > 0) {
-				content += " (" + transport + ")";
+				sb.append(" (" + transport + ")");
 			}
 			if (remark.length() > 0) {
-				content += "\n" + remark.replaceAll("\\\\n", "\n");
+				sb.append("\n" + remark.replaceAll("\\\\n", "\n"));
 			}
 			TextView tv = (TextView) view;
-			tv.setText(content);
+			tv.setText(sb.toString());
+
+			// linkify email addresses and phone numbers (e.g. 089-123456)
+			// don't linkify room numbers 00.01.123
+			Linkify.addLinks(tv, Linkify.EMAIL_ADDRESSES);
+			Linkify.addLinks(tv, Pattern.compile("[0-9-]{6,}"), "tel:");
 			return true;
 		}
 		return false;
